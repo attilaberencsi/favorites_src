5a9460292b05ecbacf7f5edabcfd5173dd1f83ee
attilaberencsi <attilaberencsi@gmail.com>
2021-08-31 14:31:46 +0200

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/otisoft/favs/Favorites/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.otisoft.favs.Favorites.controller.MainView", {});
+});
